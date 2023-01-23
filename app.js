71da96cbc9a3dfa91332670ad48dafd852da9615
Saitama <admin@example.com>
2023-01-23 16:09:10 +0300

    fast2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 export default (express, bodyParser, createReadStream, crypto, http) => {
   const app = express();
 
@@ -14,7 +16,7 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
     next();
   });
 
-  app.all("/login/", (req, res) => res.send("itmo338931"));
+  app.all("/login/", (req, res) => res.send("itmo338932"));
 
   app.all("/code/", (req, res) => {
     const filePath = import.meta.url.substring(7);
@@ -41,7 +43,21 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
     });
   });
 
-  app.all("*", (req, res) => res.send("itmo338931"));
+  const User = mongoose.model("User", { login: String, password: String });
+
+  app.post("/insert/", async (req, res) => {
+    console.log(req.body);
+    const { connection } = await mongoose.connect(req.body.URL);
+    await new User({
+      login: req.body.login,
+      password: req.body.password,
+    }).save();
+
+    connection.close();
+    res.end();
+  });
+
+  app.all("*", (req, res) => res.send("itmo338932"));
 
   return app;
 };
